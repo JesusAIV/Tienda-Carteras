e3ad8cdfb7f36db131165ca1667fa643cb010d2f
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2022-11-12 15:37:27 -0500

    update

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confecciones Milagros</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- Header -->
+    <header>
+        <!-- Barra de navegación -->
+        <nav class="navbar navbar-center">
+            <div class="navbar-center">
+                <img style="margin-right: 1rem;" src="img/png/bolso.png" alt="">
+                <a href=""><h1>Confecciones Milagros</h1></a>
+            </div>
+            <div>
+                <ul class="navbar-center">
+                    <li class="nav-item"><a href="">NOSOTROS</a></li>
+                    <li class="nav-item"><a href="">CONTACTO</a></li>
+                    <li class="nav-item"><a href="">CATEGORIAS</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="footer-center">
+            <div>
+                <div class="footer-logo">
+                    <img class="logo-img" style="margin-right: 1rem;" src="img/png/bolso.png" alt="">
+                    <a href=""><h1>Confecciones Milagros</h1></a>
+                </div>
+                <div class="caja-footer">
+                    <ul>
+                        <li>Centro comercial Luna Pizarro, Lima, La Victoria 15033</li>
+                        <li>Teléfono: +51 972351346</li>
+                        <li>Whatsapp : (+51) 982055208</li>
+                        <li>confeccionesmilagros@gmail.com</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-puntos">
+                <div>
+                    <div>
+                        <p>SIGUENOS EN</p>
+                    </div>
+                    <div class="caja-footer">
+                        <ul class="redes-sociales">
+                            <li>
+                                <a href="facebook">
+                                    <img src="img/png/facebook.png" alt="">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="instagram">
+                                    <img src="img/png/instagram.png" alt="">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="tiktok">
+                                    <img src="img/png/tik-tok.png" alt="">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <div><p>PÁGINAS</p></div>
+                    <div class="caja-footer">
+                        <ul>
+                            <li>¿Quiénes somos?</li>
+                            <li>Nuestras tiendas</li>
+                            <li>Contacto</li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <div><p>CATEGORÍAS</p></div>
+                    <div class="caja-footer">
+                        <ul>
+                            <li>Carteras</li>
+                            <li>Morrales</li>
+                            <li>Mochilas</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="derechos-reservados">
+            <p>Copyright &#169; 2022 Confecciones Milagros - Todos los derechos reservados</p>
+        </div>
+    </footer>
+</body>
+</html>
