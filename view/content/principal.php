d507be3f93bf4d24008537ce5377d476fc38fdec
Alvaro11xd <barretoalvaro369@gmail.com>
2022-11-12 17:34:24 -0500

    update

--- view/content/principal.php
+++
@@ -1,8 +0,0 @@
-<section>
-    <p>
-        Horario de atención - Lunes a Sánado 
-    </p>
-    <p>
-
-    </p>
-</section>
--- view/modulos/header.php
+++ view/modulos/header.php
@@ -1,11 +1,29 @@
 <!-- Header -->
 <header>
-    <!-- Barra de navegación -->
-    <nav class="navbar navbar-center">
-        <div class="navbar-center">
-            <img style="margin-right: 1rem;" src="img/png/bolso.png" alt="">
-            <a href=""><h1>Confecciones Milagros</h1></a>
+    <!-- Horario de Atención -->
+    <section class="horario">
+        <div class="content">
+            <p class="txt">
+                Horario de atención - Lunes a Sábado de 6:00 AM a 1:00 PM
+            </p>
+            <p class="txt">
+                +51 939 304 773
+            </p>
         </div>
+    </section>
+
+    <!-- Logo -->
+    <section class="logo">
+        <div class="">
+            <img style="margin-right: 1rem;" src="img/png/bolso.png" alt="Logo">
+            <a href="">
+                <h1>Confecciones Milagros</h1>
+            </a>
+        </div>
+    </section>
+    <!-- Barra de navegación -->
+
+    <nav class="navbar navbar-center">
         <div>
             <ul class="navbar-center">
                 <li class="nav-item"><a href="">NOSOTROS</a></li>
