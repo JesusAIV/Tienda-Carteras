40bc6e188909a825575741c0e294862481a5b44f
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2022-11-12 16:11:37 -0500

    update

--- view/modulos/header.php
+++ view/modulos/header.php
@@ -1,1 +1,17 @@
-
+    <!-- Header -->
+    <header>
+        <!-- Barra de navegación -->
+        <nav class="navbar navbar-center">
+            <div class="navbar-center">
+                <img style="margin-right: 1rem;" src="img/png/bolso.png" alt="">
+                <a href=""><h1>Confecciones Milagros</h1></a>
+            </div>
+            <div>
+                <ul class="navbar-center">
+                    <li class="nav-item"><a href="">NOSOTROS</a></li>
+                    <li class="nav-item"><a href="">CONTACTO</a></li>
+                    <li class="nav-item"><a href="">CATEGORIAS</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
