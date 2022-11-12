9528502889c695229fc194e0ad1f2d9a46fa6359
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2022-11-12 17:33:20 -0500

    update

--- view/content/tiendas.php
+++ view/content/tiendas.php
@@ -3,7 +3,24 @@
         <div>
             <p>LIMA</p>
         </div>
+        <div>
+            <div>
+                <img width="50px" src="./img/svg/mapa.svg" alt="">
+                <p>Distrito: La Victoria Lima - Lima</p>
+            </div>
+            <div>
+                <p>Tienda N° 19</p>
+            </div>
+            <div>
+                <p>Jr. Renovación N° 104, Cercado de Lima 15033</p>
+            </div>
+            <div>
+                <img width="50px" src="./img/svg/calendar.svg" alt="">
+                <p>Lunes, Martes, Miércoles, Jueves, Viernes y Sábado</p>
+            </div>
+        </div>
     </div>
     <div class="tiendas__mapa">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d243.8607766567083!2d-77.0277216!3d-12.0591731!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8bc279e3345%3A0xc237ff08b9a17f04!2sJr.%20Renovaci%C3%B3n%20104%2C%20Cercado%20de%20Lima%2015033!5e0!3m2!1ses-419!2spe!4v1668292347938!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </div>
