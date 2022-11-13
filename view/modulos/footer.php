5fbd58a6cc21846e4df383b30cbb08bb7b8e337e
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2022-11-12 23:31:57 -0500

    update

--- view/modulos/footer.php
+++ view/modulos/footer.php
@@ -1,5 +1,10 @@
     <!-- Footer -->
     <footer class="footer">
+        <div class="whatsapp">
+            <a href="https://walink.co/a2556c" target="blanck">
+                <img src="./img/svg/whatsapp.svg" alt="">
+            </a>
+        </div>
         <div class="footer-center">
             <div>
                 <div class="footer-logo">
