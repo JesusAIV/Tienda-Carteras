1cede243ba69138018de696653991d9fac8a91fc
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2022-11-12 19:21:22 -0500

    update

--- view/content/tiendas.php
+++ view/content/tiendas.php
@@ -1,30 +1,35 @@
-<div class="tiendas">
-    <div class="tiendas__info">
-        <div class="tiendas__infocontainer">
-            <div class="tiendas__title">
-                <p>LIMA</p>
-            </div>
-            <div>
-                <div class="tiendas__info--center">
-                    <img class="tiendas__icons" src="./img/svg/mapa.svg" alt="">
-                    <span>Distrito: La Victoria Lima - Lima</span>
+<div class="container__tiendas">
+    <div class="title__tiendas">
+        <p>TIENDAS</p>
+    </div>
+    <div class="tiendas">
+        <div class="tiendas__info">
+            <div class="tiendas__infocontainer">
+                <div class="tiendas__title">
+                    <p>LIMA</p>
                 </div>
-                <div class="tiendas__info--center">
-                    <img class="tiendas__icons" src="./img/svg/store.svg" alt="">
-                    <p>Tienda N° 19</p>
-                </div>
-                <div class="tiendas__info--center">
-                    <img class="tiendas__icons" src="./img/svg/mapa.svg" alt="">
-                    <p>Jr. Renovación N° 104, Cercado de Lima 15033</p>
-                </div>
-                <div class="tiendas__info--center">
-                    <img class="tiendas__icons" src="./img/svg/calendar.svg" alt="">
-                    <span>Lunes, Martes, Miércoles, Jueves, Viernes y Sábado</span>
+                <div>
+                    <div class="tiendas__info--center">
+                        <img class="tiendas__icons" src="./img/svg/mapa.svg" alt="">
+                        <span>Distrito: La Victoria Lima - Lima</span>
+                    </div>
+                    <div class="tiendas__info--center">
+                        <img class="tiendas__icons" src="./img/svg/store.svg" alt="">
+                        <p>Tienda N° 19</p>
+                    </div>
+                    <div class="tiendas__info--center">
+                        <img class="tiendas__icons" src="./img/svg/mapa.svg" alt="">
+                        <p>Jr. Renovación N° 104, Cercado de Lima 15033</p>
+                    </div>
+                    <div class="tiendas__info--center">
+                        <img class="tiendas__icons" src="./img/svg/calendar.svg" alt="">
+                        <span>Lunes, Martes, Miércoles, Jueves, Viernes y Sábado</span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="tiendas__mapa">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d243.8607766567083!2d-77.0277216!3d-12.0591731!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8bc279e3345%3A0xc237ff08b9a17f04!2sJr.%20Renovaci%C3%B3n%20104%2C%20Cercado%20de%20Lima%2015033!5e0!3m2!1ses-419!2spe!4v1668292347938!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div class="tiendas__mapa">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d243.8607766567083!2d-77.0277216!3d-12.0591731!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8bc279e3345%3A0xc237ff08b9a17f04!2sJr.%20Renovaci%C3%B3n%20104%2C%20Cercado%20de%20Lima%2015033!5e0!3m2!1ses-419!2spe!4v1668292347938!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
     </div>
 </div>
