d47cb0bd98eac488ce65755e993abc16806d99b9
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2022-11-12 20:00:11 -0500

    update

--- view/inicio.php
+++ view/inicio.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Confecciones Milagros</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/principal.css">
+    <script defer src="js/main.js"></script>
 </head>
 <body>
 
