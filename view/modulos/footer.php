61aeabf4c0601fe7307c921a61cfd786eb1ca7ec
Alvaro11xd <barretoalvaro369@gmail.com>
2022-11-13 01:48:51 -0500

    update

--- view/modulos/footer.php
+++ view/modulos/footer.php
@@ -6,11 +6,13 @@
             </a>
         </div>
         <div class="footer">
-                <div class="footer-center">
-                <div>
+            <div class="footer-center">
+                <div class="footer-datos">
                     <div class="footer-logo">
                         <img class="logo-img" style="margin-right: 1rem;" src="img/png/bolso.png" alt="">
-                        <a href=""><h1>Confecciones Milagros</h1></a>
+                        <a href="">
+                            <h1>Confecciones Milagros</h1>
+                        </a>
                     </div>
                     <div class="caja-footer">
                         <ul>
@@ -21,33 +23,36 @@
                         </ul>
                     </div>
                 </div>
+
+                <div class="footer-siguenos">
+                    <p>SIGUENOS EN</p>
+                    <div class="caja-footer">
+                        <ul class="redes-sociales">
+                            <li>
+                                <a href="facebook">
+                                    <img src="img/svg/fb.svg" alt="facebook">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="instagram">
+                                    <img src="img/svg/ig.svg" alt="">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="tiktok">
+                                    <img src="img/svg/tiktok.svg" alt="">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <p class="copy">Copyright © 2022 Confecciones Milagros - Todos los derechos reservados</p>
+                </div>
+
                 <div class="footer-puntos">
                     <div>
                         <div>
-                            <p>SIGUENOS EN</p>
+                            <p>PÁGINAS</p>
                         </div>
-                        <div class="caja-footer">
-                            <ul class="redes-sociales">
-                                <li>
-                                    <a href="facebook">
-                                        <img src="img/png/facebook.png" alt="">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="instagram">
-                                        <img src="img/png/instagram.png" alt="">
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="tiktok">
-                                        <img src="img/png/tik-tok.png" alt="">
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div>
-                        <div><p>PÁGINAS</p></div>
                         <div class="caja-footer">
                             <ul>
                                 <li><a href="quienessomos">¿Quiénes somos?</a></li>
@@ -58,7 +63,9 @@
                         </div>
                     </div>
                     <div>
-                        <div><p>CATEGORÍAS</p></div>
+                        <div>
+                            <p>CATEGORÍAS</p>
+                        </div>
                         <div class="caja-footer">
                             <ul>
                                 <li><a href="producto">Carteras</a></li>
