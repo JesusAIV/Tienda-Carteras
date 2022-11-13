661c71adf594742e920166cd209b9bab1b733fae
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2022-11-12 23:33:15 -0500

    update

--- view/modulos/footer.php
+++ view/modulos/footer.php
@@ -1,76 +1,78 @@
     <!-- Footer -->
-    <footer class="footer">
+    <footer class="">
         <div class="whatsapp">
             <a href="https://walink.co/a2556c" target="blanck">
                 <img src="./img/svg/whatsapp.svg" alt="">
             </a>
         </div>
-        <div class="footer-center">
-            <div>
-                <div class="footer-logo">
-                    <img class="logo-img" style="margin-right: 1rem;" src="img/png/bolso.png" alt="">
-                    <a href=""><h1>Confecciones Milagros</h1></a>
-                </div>
-                <div class="caja-footer">
-                    <ul>
-                        <li>Centro comercial Luna Pizarro, Lima, La Victoria 15033</li>
-                        <li>Teléfono: +51 972351346</li>
-                        <li>Whatsapp : (+51) 982055208</li>
-                        <li>confeccionesmilagros@gmail.com</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="footer-puntos">
+        <div class="footer">
+                <div class="footer-center">
                 <div>
+                    <div class="footer-logo">
+                        <img class="logo-img" style="margin-right: 1rem;" src="img/png/bolso.png" alt="">
+                        <a href=""><h1>Confecciones Milagros</h1></a>
+                    </div>
+                    <div class="caja-footer">
+                        <ul>
+                            <li>Centro comercial Luna Pizarro, Lima, La Victoria 15033</li>
+                            <li>Teléfono: +51 972351346</li>
+                            <li>Whatsapp : (+51) 982055208</li>
+                            <li>confeccionesmilagros@gmail.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="footer-puntos">
                     <div>
-                        <p>SIGUENOS EN</p>
+                        <div>
+                            <p>SIGUENOS EN</p>
+                        </div>
+                        <div class="caja-footer">
+                            <ul class="redes-sociales">
+                                <li>
+                                    <a href="facebook">
+                                        <img src="img/png/facebook.png" alt="">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="instagram">
+                                        <img src="img/png/instagram.png" alt="">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="tiktok">
+                                        <img src="img/png/tik-tok.png" alt="">
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="caja-footer">
-                        <ul class="redes-sociales">
-                            <li>
-                                <a href="facebook">
-                                    <img src="img/png/facebook.png" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="instagram">
-                                    <img src="img/png/instagram.png" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="tiktok">
-                                    <img src="img/png/tik-tok.png" alt="">
-                                </a>
-                            </li>
-                        </ul>
+                    <div>
+                        <div><p>PÁGINAS</p></div>
+                        <div class="caja-footer">
+                            <ul>
+                                <li><a href="quienessomos">¿Quiénes somos?</a></li>
+                                <li><a href="tiendas">Nuestras tiendas</a></li>
+                                <li><a href="contacto">Contacto</a></li>
+                                <li><a href="cuenta">Mi Cuenta</a></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <div><p>PÁGINAS</p></div>
-                    <div class="caja-footer">
-                        <ul>
-                            <li><a href="quienessomos">¿Quiénes somos?</a></li>
-                            <li><a href="tiendas">Nuestras tiendas</a></li>
-                            <li><a href="contacto">Contacto</a></li>
-                            <li><a href="cuenta">Mi Cuenta</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div>
-                    <div><p>CATEGORÍAS</p></div>
-                    <div class="caja-footer">
-                        <ul>
-                            <li><a href="producto">Carteras</a></li>
-                            <li><a href="producto">Morrales</a></li>
-                            <li><a href="producto">Mochilas</a></li>
-                            <li><a href="producto">Bolsos</a></li>
-                        </ul>
+                    <div>
+                        <div><p>CATEGORÍAS</p></div>
+                        <div class="caja-footer">
+                            <ul>
+                                <li><a href="producto">Carteras</a></li>
+                                <li><a href="producto">Morrales</a></li>
+                                <li><a href="producto">Mochilas</a></li>
+                                <li><a href="producto">Bolsos</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <hr>
-        <div class="derechos-reservados">
-            <p>Copyright &#169; 2022 Confecciones Milagros - Todos los derechos reservados</p>
+            <hr>
+            <div class="derechos-reservados">
+                <p>Copyright &#169; 2022 Confecciones Milagros - Todos los derechos reservados</p>
+            </div>
         </div>
     </footer>
