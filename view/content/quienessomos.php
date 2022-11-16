b2282f0368809b300e16d7bcc04af4ca5f269e2a
Alvaro11xd <110744454+Alvaro11xd@users.noreply.github.com>
2022-11-16 14:20:36 -0500

    Add files via upload

--- view/content/quienessomos.php
+++ view/content/quienessomos.php
@@ -1,2 +1,44 @@
+<main class="main__quienesSomos">
+    <section class="hero">
+        <h1 class="hero__title">CONFECCIONES MILAGROS</h1>
+        <div class="hero__quienesSomos">
+            <h2 class="hero__quienesSomos__title">¿QUIÉNES SOMOS?</h2>
+            <p class="hero__quienesSomos__paragraph">
+                Confecciones Milagros lleva ya mas de 7 años de experiencia en la industria textil y confeccione. Nos
+                caracterizamos por ofrecer a nuestro público:
+                <ul class="hero__quienesSomos__items">
+                    <li class="hero__quienesSomos__item">Productos de alta calidad</li>
+                    <li class="hero__quienesSomos__item">Finos accesorios</li>
+                    <li class="hero__quienesSomos__item">Diseños únicos</li>
+                </ul>
+            </p>
+        </div>
 
-<h1>Quienes Somos</h1>
+        <div class="hero__nuestrosProductos">
+            <h2 class="hero__nuestrosProductos__title">¿QUIÉNES SOMOS?</h2>
+            <p class="hero__nuestrosProductos__paragraph">
+                Te brindamos carteras que marcan tendencia y resaltan ese look atrevido y elegante que buscas.
+                Todas las carteras están finamente elaboradas y confeccionadas con la mas alta calidad del mercado
+                para brindarte un producto con diseños totalmente exclusivos y originales.
+            </p>
+        </div>
+
+        <div class="hero__empresa">
+            <div class="hero__empresa__mision">
+                <h2 class="hero__empresa__mision__title">MISION</h2>
+                <p class="hero__empresa__mision__paragraph">
+                    Satisfacer a nuestros clientes con productos de calidad y
+                    diseños exclusivos, superando siempre sus expectativas.
+                </p>
+            </div>
+
+            <div class="hero__empresa__vision">
+                <h2 class="hero__empresa__vision__title">VISION</h2>
+                <p class="hero__empresa__vision__paragraph">
+                    Satisfacer a nuestros clientes con productos de calidad y
+                    diseños exclusivos, superando siempre sus expectativas.
+                </p>
+            </div>
+        </div>
+    </section>
+</main>
