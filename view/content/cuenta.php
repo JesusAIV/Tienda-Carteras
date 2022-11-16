66f84ecb1dd9022d9368563ad5fc44f54b1a4124
Jesús I.V <90335295+JesusAIV@users.noreply.github.com>
2022-11-15 19:13:19 -0500

    Update cuenta.php

--- view/content/cuenta.php
+++ view/content/cuenta.php
@@ -1,2 +1,86 @@
-
-<h1>Cuenta</h1>
+<div class="micuenta">
+    <div class="cuenta-header">
+        <div class="cuenta-header-title">
+            <p>Mi Cuenta</p>
+        </div>
+        <div class="cuenta-header-subtitle">
+            <p>casa | mi cuenta</p>
+        </div>
+    </div>
+    <div class="content-cuenta">
+        <div class="form-cuenta">
+            <div class="login-cuenta" style="display: none;">
+                <div class="login-container">
+                    <div class="login-header header-cuenta">
+                        <p>Iniciar Sesión</p>
+                    </div>
+                    <div>
+                        <div>
+                            <p>Por favor complete el formulario con su correo electrónico y contraseña</p>
+                        </div>
+                        <div class="campos-form">
+                            <div>
+                                <label for="correo">Correo</label>
+                                <input type="email">
+                            </div>
+                            <div>
+                                <label for="password">Contraseña</label>
+                                <input type="password" name="password" id="password">
+                            </div>
+                            <div class="button-form">
+                                <button type="submit">Iniciar sesión</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="registro-cuenta" style="display: block;">
+                <div class="registro-container">
+                    <div class="registro-header">
+                        <p>Registrar cliente</p>
+                    </div>
+                    <div>
+                        <div>
+                            <p>Por favor complete el formulario y le crearemos una nueva cuenta en un abrir y cerrar de ojos</p>
+                        </div>
+                        <div class="campos-form">
+                            <div>
+                                <label for="nombre">Nombres</label>
+                                <input type="text">
+                            </div>
+                            <div>
+                                <label for="apellidos">Apellidos</label>
+                                <input type="text" name="apellidos" id="apellidos">
+                            </div>
+                            <div>
+                                <label for="email">Correo electrónico</label>
+                                <input type="email" name="password" id="password">
+                            </div>
+                            <div>
+                                <label for="password">Contraseña</label>
+                                <input type="password" name="password" id="password">
+                            </div>
+                            <div class="button-form">
+                                <button type="submit">Registrarse</button>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modulo-cuenta">
+            <div class="modulo-cuenta-content">
+                <div class="header-cuenta-modulo">
+                    <p>Crear nueva cuenta</p>
+                </div>
+                <div class="modulo-parrafo">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima omnis cumque eligendi hic nam assumenda quisquam ex veniam, alias exercitationem totam natus ratione eum debitis reiciendis, delectus deserunt eaque modi!</p>
+                </div>
+                <div class0="button-modulo">
+                    <button>Registrarse</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
