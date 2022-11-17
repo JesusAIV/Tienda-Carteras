48eb5f6ec51d1c51702b2c5f06d9b97852eedfc5
Alvaro11xd <barretoalvaro369@gmail.com>
2022-11-16 21:21:20 -0500

    update

--- view/content/contacto.php
+++ view/content/contacto.php
@@ -1,1 +1,45 @@
+<main class="main__contacto">
+    <picture class="mapa">
+        <iframe class="mapa__top" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d243.8607766567083!2d-77.0277216!3d-12.0591731!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8bc279e3345%3A0xc237ff08b9a17f04!2sJr.%20Renovaci%C3%B3n%20104%2C%20Cercado%20de%20Lima%2015033!5e0!3m2!1ses-419!2spe!4v1668292347938!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </picture>
 
+    <section class="contacto">
+        <div class="contacto__formulario">
+
+            <h2 class="contacto__title">COMUNICATE CON NUESTRO EQUIPO DE SOPORTE</h2>
+
+            <p class="contacto__paragraph">
+                No dudes en comunicar cualquier inquietud que tengas, Confecciones Milagros está aquí para resolver tus dudas.
+            </p>
+
+            <form action="#" method="POST" class="formulario">
+                <div class="group">
+                    <label for="nombres" class="group__label">Nombres</label>
+                    <input type="text" class="group__input" name="nombres" id="nombres">
+                </div>
+
+                <div class="group">
+                    <label for="apellidos" class="group__label">Apellidos</label>
+                    <input type="text" class="group__input" name="apellidos" id="apellidos">
+                </div>
+
+                <div class="group">
+                    <label for="celular" class="group__label">Celular</label>
+                    <input type="tel" class="group__input" name="celular" id="celular">
+                </div>
+
+                <div class="group">
+                    <label for="correo" class="group__label">Correo</label>
+                    <input type="email" class="group__input" name="correo" id="correo">
+                </div>
+
+                <textarea name="mensaje" id="mensaje" cols="90" rows="10" placeholder="Redacta tu mensaje"></textarea>
+
+                <div class="group">
+                    <button type="submit" class="group__enviar" name="correo" id="correo">Enviar Mensaje</button>
+                </div>
+            </form>
+        </div>
+    </section>
+    
+</main>
