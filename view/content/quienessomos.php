47a467d40f673517e6d6c4d2f74946dfbef800ae
Alvaro11xd <barretoalvaro369@gmail.com>
2022-11-16 19:49:30 -0500

    update

--- view/content/quienessomos.php
+++ view/content/quienessomos.php
@@ -1,9 +1,10 @@
 <main class="main__quienesSomos">
     <section class="hero">
         <h1 class="hero__title">CONFECCIONES MILAGROS</h1>
+
         <div class="hero__quienesSomos">
-            <h2 class="hero__quienesSomos__title">¿QUIÉNES SOMOS?</h2>
-            <p class="hero__quienesSomos__paragraph">
+            <h2 class="title">¿QUIÉNES SOMOS?</h2>
+            <p class="paragraph__container paragraph">
                 Confecciones Milagros lleva ya mas de 7 años de experiencia en la industria textil y confeccione. Nos
                 caracterizamos por ofrecer a nuestro público:
             <ul class="hero__quienesSomos__items">
@@ -15,8 +16,8 @@
         </div>
 
         <div class="hero__nuestrosProductos">
-            <h2 class="hero__nuestrosProductos__title">¿QUIÉNES SOMOS?</h2>
-            <p class="hero__nuestrosProductos__paragraph">
+            <h2 class="title">¿QUIÉNES SOMOS?</h2>
+            <p class="paragraph__container paragraph">
                 Te brindamos carteras que marcan tendencia y resaltan ese look atrevido y elegante que buscas.
                 Todas las carteras están finamente elaboradas y confeccionadas con la mas alta calidad del mercado
                 para brindarte un producto con diseños totalmente exclusivos y originales.
@@ -25,19 +26,20 @@
 
         <div class="hero__empresa">
             <div class="hero__empresa__mision">
-                <h2 class="hero__empresa__mision__title">MISION</h2>
-                <p class="hero__empresa__mision__paragraph">
+                <h2 class="title">MISION</h2>
+                <p class="paragraph__container paragraph">
                     Satisfacer a nuestros clientes con productos de calidad y
                     diseños exclusivos, superando siempre sus expectativas.
                 </p>
             </div>
 
             <div class="hero__empresa__vision">
-                <h2 class="hero__empresa__vision__title">VISION</h2>
-                <p class="hero__empresa__vision__paragraph">
+                <h2 class="title">VISION</h2>
+                <p class="paragraph__container paragraph">
                     Ser reconocidos a nivel nacional como la marca líder en confecciones y moda.
                 </p>
             </div>
         </div>
+
     </section>
 </main>
