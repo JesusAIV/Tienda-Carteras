dea86039c0eaecb0e6dd1210f169e68430c48f5f
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2022-11-21 11:50:01 -0500

    control de inventario

--- view/content/sesioniniciada/inventario.php
+++ view/content/sesioniniciada/inventario.php
@@ -1,1 +1,27 @@
-<h1>holaaaaa</h1>
+<div>
+    <div class="inventario-title">
+        <p>CONTROL DE INVENTARIO</p>
+    </div>
+    <div class="opc-inventario-container">
+        <a href="clientes">
+            <div class="opc-inventario-main">
+                <div class="opc-inventario-icon">
+                    <img src="./img/svg/clientes.svg" alt="clientes">
+                </div>
+                <div class="opc-inventario-title">
+                    <p>Clientes</p>
+                </div>
+            </div>
+        </a>
+        <a href="productos">
+            <div class="opc-inventario-main">
+                <div class="opc-inventario-icon">
+                    <img src="./img/svg/bag.svg" alt="bolsa">
+                </div>
+                <div class="opc-inventario-title">
+                    <p>Productos</p>
+                </div>
+            </div>
+        </a>
+    </div>
+</div>
