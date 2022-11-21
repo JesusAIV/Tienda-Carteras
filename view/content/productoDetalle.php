a5af987c0c3fa7e8cdb8296c524d02cb1e1c53c0
Alvaro11xd <barretoalvaro369@gmail.com>
2022-11-20 19:23:03 -0500

    update

--- view/content/productoDetalle.php
+++ view/content/productoDetalle.php
@@ -1,18 +1,72 @@
 <div class="principal">
-    <section class="principal__show">
-        <div class="principal__image">
-            <div class="principal__circle">
-                <img src="./img/svg/lupa.svg" alt="">
-                <img src="" alt="img_producto" class="principal__img">
-            </div>
-        </div>
-    </section>
+    <div class="detail">
 
-    <section class="principal__detail">
-        <div class="menu__navegacion">
-            <a href="principal" class="menu__navegacion__link">incio</a>
-            <span class="menu__navegacion__name">Nombre de la sección</span>
-        </div>
+        <section class="principal__show">
+            <div class="principal__image">
+                <div class="principal__circle">
+                    <img src="./img/svg/lupa.svg" alt="">
+                    <img src="" alt="img_producto" class="principal__img">
+                </div>
+            </div>
+        </section>
         
-    </section>
+        <section class="principal__detail">
+            <div class="menu__navegacion">
+                <a href="principal" class="menu__navegacion__link">incio</a>
+                <span class="menu__navegacion__name">Nombre de la sección</span>
+            </div>
+
+            <div class="detail__content">
+                <div class="detail__header">
+                    <h1 class="detail__title">Nombre del producto</h1>
+                    <p class="detail__price">S/000.00</p>
+                </div>
+        
+                <div class="detail__body">
+                    <h3 class="detail__body__title">Descripción</h3>
+                    <ul class="detail__items">
+                        <li class="detail__item">Medidas: </li>
+                        <span class="detail__item__text">19cm x 25cm</span>
+                        <li class="detail__item">Color: </li>
+                        <span class="detail__item__text">Beige</span>
+                        <li class="detail__item">Material: </li>
+                        <span class="detail__item__text">Cuero</span>
+                    </ul>
+
+                    <div class="stock">
+                        <h3 class="stock__number">200</h3>
+                        <h3 class="stock__text">disponibles</h3>
+                    </div>
+
+                    <ul class="detail__extra">
+                        <li class="detail__item">
+                            <p class="detail__name">Garantía</p>
+                            <img src="./img/svg/arrow-bottom.svg" class="detail__icon">
+                            
+                            <p class="detail__content">
+                            Este producto cuenta con una garantía por defecto de fabricación de 90 días calendario, contados a partir de la fecha de compra del producto. Para mayor información puede comunicarse a <a class="detail__references" href="mailto:confeccionesmilagros@gmail.com" target="_blank">confeccionesmilagros@gmail.com</a> o al whatsapp <a class="detail__references" href="https://wa.me/51972351346">+51972351346</a>
+                            </p>
+                        </li>
+        
+                        <li class="detail__item">
+                            <p class="detail__name">Cuidado del producto</p>
+                            <img src="./img/svg/arrow-bottom.svg" class="detail__icon">
+        
+                            <ul class="product__care">
+                                <li class="care__item">Evita exponerlo a sustancias químicas como alcohol, perfume o acetona.</li>
+                                <li class="care__item">No la guardes es espacios cerrados, húmedos ni en bolsas plásticas.</li>
+                                <li class="care__item">Límpiala usando un paño blanco húmedo sin mojar los accesorios. Si el material es PU, hazle mantenimiento una vez al mes utilizando silicona de auto en spray no graso.</li>
+                            </ul>
+
+                        </li>
+
+                    </ul>
+
+                </div>
+        
+            </div>
+
+        </section>
+        
+    </div>
 </div>
