e741f48d6ef7606d7e41b8b37c89e7d6df298c3e
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2022-11-24 01:26:20 -0500

    update

--- database/cmilagros.sql
+++ database/cmilagros.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-11-2022 a las 07:13:30
+-- Tiempo de generación: 24-11-2022 a las 07:25:11
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
