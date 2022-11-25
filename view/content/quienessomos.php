0309d7d9c41cec5c6a1be174893735b31fb1aca6
Alvaro11xd <barretoalvaro369@gmail.com>
2022-11-25 18:11:49 -0500

    update

--- view/content/quienessomos.php
+++ view/content/quienessomos.php
@@ -1,45 +1,43 @@
-<main class="main__quienesSomos">
-    <section class="hero">
-        <h1 class="hero__title">CONFECCIONES MILAGROS</h1>
+<section class="hero">
+    <h1 class="hero__title">CONFECCIONES MILAGROS</h1>
 
-        <div class="hero__quienesSomos">
-            <h2 class="title">¿QUIÉNES SOMOS?</h2>
+    <div class="hero__quienesSomos">
+        <h2 class="title">¿QUIÉNES SOMOS?</h2>
+        <p class="paragraph__container paragraph">
+            Confecciones Milagros lleva ya mas de 7 años de experiencia en la industria textil y confeccione. Nos
+            caracterizamos por ofrecer a nuestro público:
+        <ul class="hero__quienesSomos__items">
+            <li class="hero__quienesSomos__item">Productos de alta calidad</li>
+            <li class="hero__quienesSomos__item">Finos accesorios</li>
+            <li class="hero__quienesSomos__item">Diseños únicos</li>
+        </ul>
+        </p>
+    </div>
+
+    <div class="hero__nuestrosProductos">
+        <h2 class="title">NUESTROS PRODUCTOS</h2>
+        <p class="paragraph__container paragraph">
+            Te brindamos carteras que marcan tendencia y resaltan ese look atrevido y elegante que buscas.
+            Todas las carteras están finamente elaboradas y confeccionadas con la mas alta calidad del mercado
+            para brindarte un producto con diseños totalmente exclusivos y originales.
+        </p>
+    </div>
+
+    <div class="hero__empresa">
+        <div class="hero__empresa__mision">
+            <h2 class="title">MISION</h2>
             <p class="paragraph__container paragraph">
-                Confecciones Milagros lleva ya mas de 7 años de experiencia en la industria textil y confeccione. Nos
-                caracterizamos por ofrecer a nuestro público:
-            <ul class="hero__quienesSomos__items">
-                <li class="hero__quienesSomos__item">Productos de alta calidad</li>
-                <li class="hero__quienesSomos__item">Finos accesorios</li>
-                <li class="hero__quienesSomos__item">Diseños únicos</li>
-            </ul>
+                Satisfacer a nuestros clientes con productos de calidad y
+                diseños exclusivos, superando siempre sus expectativas.
             </p>
         </div>
 
-        <div class="hero__nuestrosProductos">
-            <h2 class="title">¿QUIÉNES SOMOS?</h2>
+        <div class="hero__empresa__vision">
+            <h2 class="title">VISION</h2>
             <p class="paragraph__container paragraph">
-                Te brindamos carteras que marcan tendencia y resaltan ese look atrevido y elegante que buscas.
-                Todas las carteras están finamente elaboradas y confeccionadas con la mas alta calidad del mercado
-                para brindarte un producto con diseños totalmente exclusivos y originales.
+                Ser reconocidos a nivel nacional como la marca líder en confecciones y moda.
             </p>
         </div>
+    </div>
 
-        <div class="hero__empresa">
-            <div class="hero__empresa__mision">
-                <h2 class="title">MISION</h2>
-                <p class="paragraph__container paragraph">
-                    Satisfacer a nuestros clientes con productos de calidad y
-                    diseños exclusivos, superando siempre sus expectativas.
-                </p>
-            </div>
-
-            <div class="hero__empresa__vision">
-                <h2 class="title">VISION</h2>
-                <p class="paragraph__container paragraph">
-                    Ser reconocidos a nivel nacional como la marca líder en confecciones y moda.
-                </p>
-            </div>
-        </div>
-
-    </section>
-</main>
+</section>
