01c81bc068ec5c5c8c855e9dea6192e62a35ae9b
Alvaro11xd <barretoalvaro369@gmail.com>
2022-11-25 02:05:36 -0500

    update

--- view/content/tiendas.php
+++ view/content/tiendas.php
@@ -29,7 +29,7 @@
             </div>
         </div>
         <div class="tiendas__mapa">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d243.8607766567083!2d-77.0277216!3d-12.0591731!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8bc279e3345%3A0xc237ff08b9a17f04!2sJr.%20Renovaci%C3%B3n%20104%2C%20Cercado%20de%20Lima%2015033!5e0!3m2!1ses-419!2spe!4v1668292347938!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe class="tiendas__iframe" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d243.8607766567083!2d-77.0277216!3d-12.0591731!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8bc279e3345%3A0xc237ff08b9a17f04!2sJr.%20Renovaci%C3%B3n%20104%2C%20Cercado%20de%20Lima%2015033!5e0!3m2!1ses-419!2spe!4v1668292347938!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
 </div>
